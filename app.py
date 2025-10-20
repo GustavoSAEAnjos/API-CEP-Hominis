b0514a7c6613ab2ac56b403dc59e6feb751e9c1d
Gustavo Anjos <gustavosaeanjos@gmail.com>
2025-10-20 20:57:29 -0300

    Começo da pagina de Login com mudanças no base.html

--- app.py
+++ app.py
@@ -1,57 +1,119 @@
-# Importação das bibliotecas essenciais para a aplicação web
-# Flask: framework principal para criação de rotas, renderização de templates e manipulação de requisições
-# SQLAlchemy: biblioteca ORM para interação com banco de dados relacional
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
-# Inicialização da aplicação Flask e configuração do banco de dados SQLite
+
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cursos.sqlite3"
-app.secret_key = "crud_cursos_super_secreta_e_forte"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///hominis.sqlite3"
+app.secret_key = "senha_padrao_projeto_flask_hominis_12345"
 db = SQLAlchemy(app)
 
-# Modelo de dados: Curso
-class Curso(db.Model):
-    id_curso = db.Column(db.Integer, primary_key=True)
-    nome_curso = db.Column(db.String(40), nullable=False)
-    descricao = db.Column(db.String(120), nullable=False)
-    carga_horaria = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, nome_curso, descricao, carga_horaria):
-        self.nome_curso = nome_curso
-        self.descricao = descricao
-        self.carga_horaria = carga_horaria
+class hominis(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.Text, nullable=False)
+    cpf = db.Column(db.Text, nullable=False)
+    data_nasc_fund = db.Column(db.Text, nullable=False)
+    genero = db.Column(db.Text, nullable=True)
+    estado_civil = db.Column(db.Text, nullable=True)
+    nacionalidade = db.Column(db.Text, nullable=True)
+    ocupaçao = db.Column(db.Text, nullable=False)
+    telefone_principal = db.Column(db.Text, nullable=False)
+    telefone_secundario = db.Column(db.Text, nullable=True)
+    email_prncipal = db.Column(db.Text, nullable=False)
+    email_secundario = db.Column(db.Text, nullable=True)
+    cep = db.Column(db.Text, nullable=False)
+    logradouro = db.Column(db.Text, nullable=False)
+    numero_casa = db.Column(db.Text, nullable=False)
+    complemento = db.Column(db.Text, nullable=True)
+    bairro = db.Column(db.Text, nullable=False)
+    cidade = db.Column(db.Text, nullable=False)
+    estado = db.Column(db.Text, nullable=False)
+    pais = db.Column(db.Text, nullable=False)
 
-# Rota inicial
-@app.route("/")
+    def __init__(self, nome, cpf, data_nasc_fund, genero, estado_civil, nacionalidade,
+                 ocupaçao, telefone_principal, telefone_secundario, email_prncipal,
+                 email_secundario, cep, logradouro, numero_casa, complemento,
+                 bairro, cidade, estado, pais):
+        self.nome = nome
+        self.cpf = cpf
+        self.data_nasc_fund = data_nasc_fund
+        self.genero = genero
+        self.estado_civil = estado_civil
+        self.nacionalidade = nacionalidade
+        self.ocupaçao = ocupaçao
+        self.telefone_principal = telefone_principal
+        self.telefone_secundario = telefone_secundario
+        self.email_prncipal = email_prncipal
+        self.email_secundario = email_secundario
+        self.cep = cep
+        self.logradouro = logradouro
+        self.numero_casa = numero_casa
+        self.complemento = complemento
+        self.bairro = bairro
+        self.cidade = cidade
+        self.estado = estado
+        self.pais = pais
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        return redirect(url_for("principal"))
+
+    return render_template("login.html")
+
+@app.route("/index")
 def principal():
     return render_template("index.html")
 
-# Listagem paginada de cursos
+
 @app.route("/listar")
 def listar():
     return render_template("listar.html")
 
-# Cadastro de novo curso
+
 @app.route("/cadastrar", methods=["GET", "POST"])
 def cadastrar():
     return render_template("cadastrar.html")
 
-# Exclusão de curso
+
 @app.route("/excluir", methods=["GET", "POST"])
 def excluir():
     return redirect(url_for("listar"))
 
-# Edição de curso
+
 @app.route("/editar", methods=["GET", "POST"])
 def editar():
     return render_template("editar.html")
 
-# Inicialização da aplicação
+
 if __name__ == "__main__":
     # Cria as tabelas no banco caso ainda não existam
     with app.app_context():
         db.create_all()
+        admin = hominis.query.filter_by(cpf='00000000000').first()
+        if not admin:
+            admin = hominis(
+                nome="Administrador",
+                cpf="00000000000",
+                data_nasc_fund="0000-00-00",
+                genero="Nenhum",
+                estado_civil="Nenhum",
+                nacionalidade="Nenhuma",
+                ocupaçao="Administrador",
+                telefone_principal="(00) 00000-0000",
+                telefone_secundario=None,
+                email_prncipal="Nenhum",
+                email_secundario=None,
+                cep="00000-000",
+                logradouro="Nenhum",
+                numero_casa="0",
+                complemento=None,
+                bairro="Nenhum",
+                cidade="Nenhum",
+                estado="Nenhum",
+                pais="Nenhum"
+            )
+            db.session.add(admin)
+            db.session.commit()
 
-    # Inicia o servidor Flask em modo de desenvolvimento
     app.run(debug=True)
